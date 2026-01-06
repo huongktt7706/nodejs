05681d381e75dc0bc7094616282b89b05957a80e
huongktt7706 <thuyhuong7706nb@gmail.com>
2026-01-06 13:40:28 +0700

    aa

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", () => {
+    console.log("Home page");
+});
+
+app.listen(3000, () => {
+    console.log("Server đang chạy cổng 3000");
+});
